990ebcaa4f22e1e82e2185c6a7782f84efd5cd69
Professor JG <113101211+professorjg@users.noreply.github.com>
2022-11-18 06:22:48 -0500

    Initial commit

---
+++ src/edu/wit/cs/comp1000/PA6a.java
@@ -0,0 +1,49 @@
+package edu.wit.cs.comp1000;
+
+// TODO: document this class
+public class PA6a {
+	
+	/**
+	 * Error to output if year is not positive
+	 */
+	static final String E_YEAR = "The year must be positive!";
+	
+	/**
+	 * Error to output if the day is not between 0 and 6
+	 */
+	static final String E_DAY = "The day of January 1st must be between 0 and 6!";
+	
+	/**
+	 * Determines if an input is a leap year
+	 * 
+	 * @param year year in question
+	 * @return true if a leap year
+	 */
+	public static boolean isLeapYear(int year) {
+		return false; // TODO: replace with your code
+	}
+	
+	/**
+	 * Outputs a month to the console
+	 * 
+	 * @param month title
+	 * @param startDay 0=Sunday ... 6=Saturday
+	 * @param numDays number of days in the month
+	 * @return day of the week after the last day of the month
+	 */
+	public static int printMonth(String month, int startDay, int numDays) {
+		return 0; // TODO: replace with your code
+	}
+
+	/**
+	 * Program execution point:
+	 * input year, day of the week (0-6) of january 1
+	 * output calendar for that year
+	 * 
+	 * @param args command-line arguments (ignored)
+	 */
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+}
